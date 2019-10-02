4217d8f32836f114810a7f5b7def441d7cffafbd
Jiggy MiNi <jiggymini@hotmail.com>
2019-10-02 08:12:19 +0000

    rename message

--- hello-world.go
+++ hello-world.go
@@ -11,7 +11,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Hello world received a request.")
 	target := os.Getenv("TARGET")
 	if target == "" {
-		target = "World by Jiggy MiNi ECR"
+		target = "World by Jiggy MiNi ECR CircleCI"
 	}
 	fmt.Fprintf(w, "Hello %s!", target)
 }
