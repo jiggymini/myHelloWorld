dd0cb6b3d714a776eb3620317fab73f6345a5ff4
Jiggy MiNi <jiggymini@hotmail.com>
2019-10-02 06:53:55 +0000

    add ecr repo

--- hello-world.go
+++ hello-world.go
@@ -11,7 +11,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Hello world received a request.")
 	target := os.Getenv("TARGET")
 	if target == "" {
-		target = "World by Jiggy MiNi"
+		target = "World by Jiggy MiNi ECR"
 	}
 	fmt.Fprintf(w, "Hello %s!", target)
 }
--- hello-world_test.go
+++ hello-world_test.go
@@ -29,7 +29,7 @@ func TestHandler(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `Hello World by Jiggy MiNi!`
+	expected := `Hello World by Jiggy MiNi ECR!`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
